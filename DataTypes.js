13acbcbecda36e07ea58309f7c284b5d62b34143
satish-kumar60 <satisverma8651@gmail.com>
2025-07-07 21:05:10 +0530

    DataTypes

---
+++ DataTypes.js
@@ -0,0 +1,46 @@
+"use strict";//treat6 all js code as newer version
+//alert("Hello Satish Verma"); //we are ushing nodejs , not browser
+console.log(3+3);
+console.log("Hello Satish Verma");
+
+
+let name = "Satish Verma" //string
+let age = 30 //number 
+let isMarried = false //boolean
+let state; //undefined
+let address = null //null
+
+
+// Data Types in JavaScript
+// 1. Primitive Data Types
+// 2. Non-Primitive Data Types
+// Primitive Data Types
+// 1. String: Represents a sequence of characters, enclosed in single or double quotes.
+// 2. Number: Represents numeric values, both integers and floating-point numbers.
+// 3. Boolean: Represents a logical entity and can have two values: true or false.
+// 4. Null: Represents the intentional absence of any object value.
+// 5. Undefined: Represents a variable that has been declared but has not yet been assigned a value.
+// 6. Symbol: Represents a unique and immutable value, often used as object property keys
+
+// Non-Primitive Data Types
+// 1. Object: Represents a collection of key-value pairs, where keys are strings (  or Symbols) and values can be of any data type.
+// 2. Array: A special type of object that represents a list-like collection of values, where each value can be of any data type.   
+// 3. Function: A special type of object that can be called to perform a specific task or computation.
+// 4. Date: Represents a specific point in time, including date and time information.
+// 5. RegExp: Represents a regular expression, which is used for pattern matching in strings.
+// 6. Map: Represents a collection of key-value pairs, where keys can be of any data type and values can also be of any data type.
+// 7. Set: Represents a collection of unique values, where each value can be of any data type.
+
+//number => 2 to power 53
+//bigint 
+//string  => ""
+//boolean => true/false
+//null => empty value standlone value
+//undefined => variable is not defined
+//symbol => unique value
+
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+
