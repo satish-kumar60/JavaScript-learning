24dc1cd4e561ed435f3c620d567bc4144face293
satish-kumar60 <satisverma8651@gmail.com>
2025-07-07 19:59:08 +0530

    Gain somthing

---
+++ variable.js
@@ -0,0 +1,16 @@
+const accountId=4586128
+let accountEmail="Satishverma8651@gmail.com"
+var accountPassword="1234567890"
+accountCity="Delhi"
+let accountState;
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+
+
+accountEmail="Satishverma@gmail.com"
+accountPassword="54834567890"
+accountCity="jharkhand"
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
